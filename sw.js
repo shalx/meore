e4058e4565b39c2680eecf9563f1e7a16bc4903e
shalx <shalva@inbox.com>
2026-06-18 05:07:41 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,23 @@
-const CACHE_NAME = 'fixpin-v6-cache';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.15.0/maps/maps.css',
-  'https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.15.0/maps/maps-web.min.js'
+const CACHE_NAME="fixpin-v7";
+
+const FILES=[
+"./",
+"./index.html",
+"./manifest.json"
 ];
 
-// Установка воркера - скачиваем ресурсы в кэш
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    }).then(() => self.skipWaiting())
-  );
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(CACHE_NAME).then(cache=>{
+return cache.addAll(FILES);
+})
+);
 });
 
-// Активация - удаляем старый кэш, если обновили версию
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
-});
-
-// Стратегия: Сначала проверяем сеть. Если сети нет — берем из кэша.
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => {
-      return caches.match(e.request);
-    })
-  );
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(r=>{
+return r || fetch(e.request);
+})
+);
 });
