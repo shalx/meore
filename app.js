156adc3b88d36baca7eea347c29b857dc97fa61f
shalx <shalva@inbox.com>
2026-07-14 16:56:12 +0400

    Update app.js

--- app.js
+++ app.js
@@ -353,19 +353,56 @@ renderLocations();
 
 function renderLocations(){
 
+    let box = document.getElementById("locations-list");
 
-let box =
-document.getElementById("locations-list");
+    if(savedLocations.length===0){
+        box.innerHTML = "Список пуст.";
+        return;
+    }
 
+    box.innerHTML="";
 
-if(savedLocations.length===0){
+    savedLocations.forEach(x=>{
 
-box.innerHTML =
-"Список пуст.";
+        box.innerHTML += `
+        <div>
 
-return;
+        <b>${x.note || "Without a note"}</b><br>
+
+        ${x.address || "N/A"}<br>
+
+        Lat: ${x.lat}<br>
+
+        Lng: ${x.lng}<br>
+
+        Altitude: ${x.altitude} m<br>
+
+        Temp: ${x.temperature} °C<br>
+
+        Wind: ${x.windSpeed} km/h<br>
+
+        ${x.time}
+
+        <br>
+
+        <!-- Кнопка перехода на Google Maps -->
+        <button onclick="goToLocation(${x.lat}, ${x.lng})">
+            ПОКАЗАТЬ НА КАРТЕ
+        </button>
+
+        <button onclick="deleteLocation(${x.id})">
+            DELETE
+        </button>
+
+        <hr>
+
+        </div>
+        `;
+
+    });
 
 }
+
 
 
 
