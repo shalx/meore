4117dc3d1db4952e378dba1de951fbb88357698a
shalx <shalva@inbox.com>
2026-07-15 16:39:07 +0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
+let currentLocation = null;
 
+document.addEventListener(
+    "DOMContentLoaded",
+    init
+);
+
+function init() {
+
+    document
+        .getElementById("pin-btn")
+        .addEventListener("click", getLocation);
+
+    document
+        .getElementById("save-btn")
+        .addEventListener("click", saveLocation);
+
+    document
+        .getElementById("tracker-btn")
+        .addEventListener("click", openTracker);
+
+    renderLocations();
+
+}
