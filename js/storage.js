665d96a4d4cb5a88459dc1c70c4f0478ced466e3
shalx <shalva@inbox.com>
2026-07-15 16:39:25 +0400

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,28 @@
+const STORAGE_KEY = "meore_locations";
 
+let savedLocations = loadLocations();
+
+function loadLocations() {
+
+    try {
+
+        return JSON.parse(
+            localStorage.getItem(STORAGE_KEY)
+        ) || [];
+
+    } catch {
+
+        return [];
+
+    }
+
+}
+
+function saveLocations() {
+
+    localStorage.setItem(
+        STORAGE_KEY,
+        JSON.stringify(savedLocations)
+    );
+
+}
