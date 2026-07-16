3b68a8add73ecac5f37833eba409d5ddc1418be8
shalx <shalva@inbox.com>
2026-07-16 22:17:17 +0400

    Update events.js

--- tracker/js/events.js
+++ tracker/js/events.js
@@ -1,1 +1,86 @@
+// =====================================
+// Tracker Night
+// events.js
+// User Events
+// =====================================
 
+window.Tracker = window.Tracker || {};
+
+Tracker.events = {
+
+    init() {
+
+        // START
+
+        const startBtn = document.getElementById("startBtn");
+
+        if (startBtn) {
+
+            startBtn.addEventListener("click", () => {
+
+                Tracker.tracker.start();
+
+            });
+
+        }
+
+        // STOP
+
+        const stopBtn = document.getElementById("stopBtn");
+
+        if (stopBtn) {
+
+            stopBtn.addEventListener("click", () => {
+
+                Tracker.tracker.stop();
+
+            });
+
+        }
+
+        // SAVE ROUTE
+
+        const saveBtn = document.getElementById("saveBtn");
+
+        if (saveBtn) {
+
+            saveBtn.addEventListener("click", () => {
+
+                Tracker.storage.saveCurrentTrack();
+
+            });
+
+        }
+
+        // BACK
+
+        const backBtn = document.getElementById("backBtn");
+
+        if (backBtn) {
+
+            backBtn.addEventListener("click", () => {
+
+                window.location.href = "../";
+
+            });
+
+        }
+
+        // Interval
+
+        const interval = document.getElementById("interval");
+
+        if (interval) {
+
+            interval.addEventListener("change", (e) => {
+
+                Tracker.config.defaultInterval =
+                    parseInt(e.target.value);
+
+            });
+
+        }
+
+    }
+
+};
