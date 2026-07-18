eb103f9b002e53414f596ea1e2565649e6e5260d
shalx <shalva@inbox.com>
2026-07-18 05:18:49 +0400

    Update route.js

--- tracker/route.js
+++ tracker/route.js
@@ -1,9 +1,1 @@
-const DEFAULT_CONFIG = {
-    stopConfirmationTime: 3 * 60 * 1000,
-    candidateRadius: 35,
-    stopExitRadius: 70,
-    maxAccuracy: 100,
-    minPositionInterval: 5000,
-    storageKey: "tracker_night_state",
-    autoSave: true
-};
+
