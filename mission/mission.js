0021879cad6b02c563431a63bea7881ec3b23ff4
shalx <shalva@inbox.com>
2026-07-18 05:57:56 +0400

    Update mission.js

--- mission/mission.js
+++ mission/mission.js
@@ -1,1 +1,35 @@
+──────────────────────────────
+            MISSION
+──────────────────────────────
 
+Progress
+0 / 0 completed
+
+──────────────────────────────
+
+☐ Home
+
+Lat: 41.675094
+Lng: 44.919192
+
+Status
+NOT STARTED
+
+──────────────────────────────
+
+☐ Pharmacy
+
+Lat: ...
+
+Status
+NOT STARTED
+
+──────────────────────────────
+
+START MISSION
+
+STOP
+
+RESET
+
+BACK
